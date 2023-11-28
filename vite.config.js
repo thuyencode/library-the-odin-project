170d4f0a7be5440c3956a8ce73b45ac9624b94f3
thuyencode <thuyencode@proton.me>
2023-11-28 22:13:23 +0700

    Use vite-plugin-webfont-dl to bundle webfonts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import simpleHtmlPlugin from 'vite-plugin-simple-html'
+import { webfontDownload } from 'vite-plugin-webfont-dl'
 
 export default defineConfig({
   base: '/library-the-odin-project/',
-  plugins: [simpleHtmlPlugin({ minify: true })],
+  plugins: [simpleHtmlPlugin({ minify: true }), webfontDownload()],
 })
