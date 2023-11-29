493cd3f4bc05b259313d6be98b881bf3bfa46349
thuyencode <thuyencode@proton.me>
2023-11-29 10:57:25 +0700

    Add Preline UI to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,17 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/preline/dist/*.js'
+  ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
-      },
-    },
+        sans: ['Inter', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
-  plugins: [],
+  plugins: [require('preline/plugin')]
 }
